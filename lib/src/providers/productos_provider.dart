38d824f42dd71bff37d951cce10c89eda570c019
Omar David <oder1011119504@gmail.com>
2020-02-16 21:55:29 -0400

    archivos faltantes en inicio de version 2

---
+++ lib/src/providers/productos_provider.dart
@@ -0,0 +1,13 @@
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+
+import 'package:mime_type/mime_type.dart';
+
+import 'package:formvalidation/src/models/producto_model.dart';
+
+
+
